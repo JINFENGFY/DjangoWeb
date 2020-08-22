3ce0b62c8e292a06089aa553a5264f11d49e3b1d
JINFENGFY <1058023027@qq.com>
2020-08-22 14:55:52 +0800

    对模型类做了一些小修改

--- learning_log/models.py
+++ learning_log/models.py
@@ -12,7 +12,7 @@ class Category (models.Model):
 
 
     def __str__(self):
-        return u'Category:%s' % self.category
+        return u'类型:%s' % self.category
 
 #学习笔记主体模型
 class LearningContent(models.Model):
@@ -20,11 +20,11 @@ class LearningContent(models.Model):
     title=models.CharField(max_length=30,verbose_name=u'题目')
     content=models.TextField(verbose_name=u'内容')
     createdTime=models.DateTimeField(default=timezone.now(),verbose_name=u'创建时间')
-    owner=models.ForeignKey(User,on_delete=models.CASCADE,verbose_name=u'所有者')
+    owner=models.ForeignKey(User,on_delete=models.CASCADE,related_name='learning_log',verbose_name=u'所有者')
     categories=models.ManyToManyField(Category,verbose_name=u'类型')
 
     def __str__(self):
-        return u'Learning_log:%s--%s' % (self.title, self.owner)
+        return u'学习笔记:%s--%s' % (self.title, self.owner)
 
 #评论模型
 class Comment(models.Model):
