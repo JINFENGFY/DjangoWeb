0bf1872509d3f62add91f9a746d129360fb3e38e
JINFENGFY <1058023027@qq.com>
2020-08-23 14:36:07 +0800

    显示所有读书笔记功能

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -6,6 +6,9 @@ from learning_log import views
 
 app_name='learning_log'
 urlpatterns =[
-    #显示主页，显示所有项目
-    url(r'',views.Index.as_view(),name='index'),
+    #显示主页
+    url(r'^$',views.Index.as_view(),name='index'),
+
+    #显示所有学习笔记
+    url(r'^topic/',views.Topic.as_view(),name='topic')
 ]
--- learning_log/views.py
+++ learning_log/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import *
-
-# Create your views here.
-#显示主页，显示所有项目
 from django.views import View
+# Create your views here.
 
-
+#显示主页
 class Index (View):
     def get(self,request):
-        post = LearningContent.objects.all ()
-        return render (request, 'index.html', {'post': post})
+        return render (request, 'index.html')
+
+#显示所有项目
+class Topic (View):
+    def get(self,request):
+        topics=LearningContent.objects.all().order_by('-createdTime')
+        return render(request,'topics.html',{'topics':topics})
