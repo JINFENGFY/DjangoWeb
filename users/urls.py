cb4c9a76c7ae23670d143230950928df3aa50717
JINFENGFY <1058023027@qq.com>
2020-08-25 14:38:39 +0800

    注册功能

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,14 @@
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView, LogoutView
 
+from users import views
+
 app_name='users'
 
 urlpatterns =[
+    #用户注册
+    url(r'^register/$',views.Register.as_view(),name='register'),
+
     #登录页面
     url(r'^login/$',LoginView.as_view(template_name='login.html'),name='login'),
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.urls import reverse
+
 
 # Create your views here.
+class Register (View):
+    form1 = UserCreationForm ()
+
+    def get(self,request):
+        return render(request,'register.html',{'form':self.form1})
+
+    def post(self,request):
+        form=UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user=form.save()
+
+            #自动登录
+            authenticated_user=authenticate(username=new_user.username,
+                                            password=request.POST['password1'])
+            login(request,authenticated_user)
+            return HttpResponseRedirect(reverse('learning_log:index'))
+        return render(request,'register.html',{'form':self.form1})
