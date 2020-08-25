8f35ecf6d7a75b3692e7b1a7a12c726862be4a2e
JINFENGFY <1058023027@qq.com>
2020-08-25 11:18:56 +0800

    用户登录功能

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 #coding=utf-8
 
 from django.conf.urls import url
+from django.contrib.auth.views import LoginView
 
-urlpattern=[
+app_name='users'
 
+urlpatterns =[
+    #登录页面
+    url(r'^login/$',LoginView.as_view(template_name='login.html'),name='login')
 ]
