fb150b6c379492bed927874e510606b20031bb12
JINFENGFY <1058023027@qq.com>
2020-09-03 08:00:00 +0800

    全局上下文，用于显示侧方导航栏

---
+++ learning_log/my_context_processor.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+from django.db.models import Count
+
+from .models import *
+
+def Side_navigation_bar_data(request):
+    #分类归档
+    categories=LearningContent.objects.values('categories__category','categories')\
+        .annotate(c=Count('*')).order_by('-c')
+
+    #近期文章
+    recent_log=LearningContent.objects.all().order_by('-createdTime')[0:5]
+
+    return {'Side_nav_categories':categories,'Side_nav_recentlog':recent_log}
