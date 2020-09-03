c89e59038c6349266858499dceb67eb7ce9403f5
JINFENGFY <1058023027@qq.com>
2020-09-03 08:02:10 +0800

    添加了一套专门用于学习笔记的模板页用于显示更加详细的内容，原先的模板页更加简化，方便后续添加更多功能

--- DjangoWeb/settings.py
+++ DjangoWeb/settings.py
@@ -71,6 +71,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #侧边栏全局上下文
+                'learning_log.my_context_processor.Side_navigation_bar_data',
             ],
         },
     },
@@ -142,40 +144,6 @@ STATIC_URL = '/static/'
 LOGOUT_REDIRECT_URL='learning_log:index'
 #设置访问限制路径
 LOGIN_URL = 'learning_log:index'
-
-#富文本编辑器配置
-# TINYMCE_DEFAULT_CONFIG = {
-#     # // General options
-#     'mode': 'textareas',
-#     'theme': "advanced",
-#     'plugins': "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave",
-#
-#     # // Theme  options
-#     'theme_advanced_buttons1': "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,fullscreen,code",
-#     'theme_advanced_buttons2': "cut,copy,paste,pastetext,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-#     'theme_advanced_buttons3': "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl",
-#     'theme_advanced_toolbar_location': "top",
-#     'theme_advanced_toolbar_align': "left",
-#     'theme_advanced_statusbar_location': "bottom",
-#     'theme_advanced_resizing': 'true',
-#
-#     # // content_css: "/css/style.css",
-#     'template_external_list_url': "lists/template_list.js",
-#     'external_link_list_url': "lists/link_list.js",
-#     'external_image_list_url': "lists/image_list.js",
-#     'media_external_list_url': "lists/media_list.js",
-#
-# # // Style formats
-#     'style_formats': [
-#     {'title': 'Bold text', 'inline': 'strong'},
-#     {'title': 'Red text', 'inline': 'span', 'styles': {'color': '#ff0000'}},
-#     {'title': 'Help', 'inline': 'strong', 'classes': 'help'},
-#     {'title': 'Table styles'},
-#     {'title': 'Table row 1', 'selector': 'tr', 'classes': 'tablerow'}
-#     ],
-#     'width': '700',
-#     'height': '400'
-# }
 
 #检索分词文件路径
 HAYSTACK_CONNECTIONS = {
--- learning_log/templatetags/Custom_filters.py
+++ learning_log/templatetags/Custom_filters.py
@@ -13,3 +13,11 @@ def split_str(value,args):
     if end=='':
         return result[int (start):length]
     return result[int(start):int(end)]
+
+#剪切字符串，剩余部分用省略号代替
+@register.filter
+def split_str_cut(value,args):
+    lens=int(args)
+    result=str(value)
+    length=len(result)
+    return result[:lens]+'...'
