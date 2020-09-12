863c3fce93cbbd710183b9ec9a51b464cfdf1988
JINFENGFY <1058023027@qq.com>
2020-09-12 14:42:32 +0800

    在管理后台中将用户拓展信息内嵌到User中

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import User_more_info
 
-# Register your models here.
+# 定义一个行内 admin
+class MoreInfoInline(admin.StackedInline):
+    model = User_more_info
+    can_delete = False
+    verbose_name_plural = 'User_more_info'
+
+# 将 MoreInfoInline 关联到 User 中
+class UserAdmin(BaseUserAdmin):
+    inlines = (MoreInfoInline,)
+
+# 重新注册 User
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
