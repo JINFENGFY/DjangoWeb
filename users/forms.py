cb7ee5a3b08c292a679d4053da7f3fd636f9b82d
JINFENGFY <1058023027@qq.com>
2020-09-13 09:19:08 +0800

    拓展了UserCreateFrom添加emali字段

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,28 @@
 #coding=utf-8
+from django.contrib.auth.models import User
+
 from .models import User_more_info
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 class MoreInfoFrom(forms.ModelForm):
     class Meta:
         model=User_more_info
         fields=('gender','phone','brief_introduction')
+
+class UserCreationExpandFrom(UserCreationForm):
+    email=forms.EmailField(label=("邮箱"),
+                            widget=forms.EmailInput,
+                           help_text='为方便找回密码，请输入一个邮箱'
+                           )
+
+    class Meta:
+        model = User
+        fields = ("username", "email",)
+
+    def save(self, commit=True):
+        user = super (UserCreationExpandFrom, self).save (commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save ()
+        return user
