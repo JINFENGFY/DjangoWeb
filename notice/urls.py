dd89058ac66a29158fbd66ec10ac47286fbc2cf5
JINFENGFY <1058023027@qq.com>
2020-09-13 17:13:31 +0800

    代码格式化以及添加必要的注释

--- notice/urls.py
+++ notice/urls.py
@@ -1,10 +1,11 @@
-#coding=utf-8
+# coding=utf-8
 from django.conf.urls import url
 from . import views
 
-app_name='notice'
-
-urlpatterns=[# 通知列表
+app_name = 'notice'
+urlpatterns = [
+    # 通知列表
     url ('list/', views.CommentNoticeListView.as_view (), name='list'),
+    #更新通知
     url ('update/', views.CommentNoticeUpdateView.as_view (), name='update'),
 ]
--- notice/views.py
+++ notice/views.py
@@ -1,14 +1,16 @@
-#coding=utf-8
+# coding=utf-8
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.generic import ListView
 from learning_log.models import LearningContent
 from notifications.models import *
 
-
-class CommentNoticeListView(LoginRequiredMixin, ListView):
+@method_decorator (login_required (), name='dispatch')
+class CommentNoticeListView (LoginRequiredMixin, ListView):
     """通知列表"""
     # 上下文的名称
     context_object_name = 'notices'
@@ -21,24 +23,25 @@ class CommentNoticeListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return self.request.user.notifications.unread ()
 
-
-class CommentNoticeUpdateView(View):
+@method_decorator (login_required (), name='dispatch')
+class CommentNoticeUpdateView (View):
     """更新通知状态"""
+
     # 处理 get 请求
     def get(self, request):
         # 获取未读消息
-        notice_id = request.GET.get('notice_id')
+        notice_id = request.GET.get ('notice_id')
 
-        #删除数据库中的已读记录
-        Notification.objects.filter(unread=0).delete()
+        # 删除数据库中的已读记录
+        Notification.objects.filter (unread=0).delete ()
 
         # 更新单条通知
         if notice_id:
-            learning_log = LearningContent.objects.get(lnum=request.GET.get('topic_id'))
-            request.user.notifications.get(id=notice_id).mark_as_read()
+            learning_log = LearningContent.objects.get (lnum=request.GET.get ('topic_id'))
+            request.user.notifications.get (id=notice_id).mark_as_read ()
 
-            return redirect(reverse('learning_log:topic',args=[learning_log.lnum]))
+            return redirect (reverse ('learning_log:topic', args=[learning_log.lnum]))
         # 更新全部通知
         else:
-            request.user.notifications.mark_all_as_read()
-            return redirect(reverse('learning_log:topics',args=[1]))
+            request.user.notifications.mark_all_as_read ()
+            return redirect (reverse ('learning_log:topics', args=[1]))
